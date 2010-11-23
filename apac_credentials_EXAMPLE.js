6b69e17dbd4c986221b957aff64affd7427c864f
Greg Heartsfield <scsibug@imap.cc>
2010-11-23 14:51:19 -0600

    Added file with example amazon credentials.

---
+++ apac_credentials_EXAMPLE.js
@@ -0,0 +1,5 @@
+// Amazon credentials should be specified here.
+// This file must then be renamed "apac_credentials.js"
+exports.awsId = '[AWS Account ID]';
+exports.awsSecret = '[AWS Secret]';
+exports.assocId = '[Tracking ID]';
