108292462f1922fdc3c656a54d4fc0e6516a2bec
Greg Heartsfield <scsibug@imap.cc>
2010-11-22 22:56:50 -0600

    Formatting.

--- 52-in-52.js
+++ 52-in-52.js
@@ -4,7 +4,18 @@ var sys = require('sys');
 var client = require("./lib/redis-client").createClient();
 
 app.get('/', function(req, res){
-        res.send('hello world');
+    res.send('hello world');
+});
+
+app.get('/users', function(req, res){
+    var userlist = "User List\n"
+    client.keys("user:*:name", function(err,reply){
+        sys.print(reply);
+        userlist=String(reply)
+        res.send(userlist);
+        client.get(reply, function(err,reply){
+        });
     });
+});
 
 app.listen(8124);
