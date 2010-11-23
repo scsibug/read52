ca0cc5cc28151b5d7ed203c8c55b8d66a7ce3a26
Greg Heartsfield <scsibug@imap.cc>
2010-11-23 01:33:48 -0600

    User listing page works.

--- 52-in-52.js
+++ 52-in-52.js
@@ -3,19 +3,30 @@ var sys = require('sys');
 
 var client = require("./lib/redis-client").createClient();
 
+app.listen(8124);
+
 app.get('/', function(req, res){
     res.send('hello world');
 });
 
 app.get('/users', function(req, res){
-    var userlist = "User List\n"
-    client.keys("user:*:name", function(err,reply){
-        sys.print(reply);
-        userlist=String(reply)
-        res.send(userlist);
-        client.get(reply, function(err,reply){
-        });
-    });
+    var userlist = "<h1>User List</h1><br />"
+    listUsers(client,
+              function(name) {userlist+=String(name+"<br />")},
+              function() {res.send(userlist)}
+             );
 });
 
-app.listen(8124);
+// sends user names to userCallback, endCallback called at completion
+var listUsers = function(client, userCallback, endCallback) {
+    client.keys("user:*:name", function(err,reply){
+        var replies = 0;
+        for(var i=0; i < reply.length; i++) {
+            client.get(reply[i], function(err,name){
+                 userCallback(name);
+                replies++;
+                if (replies == reply.length) {endCallback();}
+            });
+        }
+    });
+}
