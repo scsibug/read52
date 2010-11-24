5754e6b0a836e60df776e117035b9938cad615bd
Greg Heartsfield <scsibug@imap.cc>
2010-11-24 16:52:41 -0600

    Moved user listing function to users module.

--- 52-in-52.js
+++ 52-in-52.js
@@ -10,14 +10,17 @@ app.get('/', function(req, res){
     res.send('hello world');
 });
 
+// List All Users
 app.get('/users', function(req, res){
     var userlist = "<h1>User List</h1><br />"
-    listUsers(client,
-              function(name) {userlist+=String(name+"<br />")},
-              function() {res.send(userlist)}
-             );
+    users.listUsers(
+        client,
+        function(name) {userlist+=String(name+"<br />")},
+        function() {res.send(userlist)}
+    );
 });
 
+// List All Books
 app.get('/books', function(req, res){
     var booklist = "<h1>Book List</h1><br />"
     books.listBooks(client,
@@ -26,16 +29,3 @@ app.get('/books', function(req, res){
              );
 });
 
-// sends user names to userCallback, endCallback called at completion
-var listUsers = function(client, userCallback, endCallback) {
-    client.keys("user:*:name", function(err,reply){
-        var replies = 0;
-        for(var i=0; i < reply.length; i++) {
-            client.get(reply[i], function(err,name){
-                 userCallback(name);
-                replies++;
-                if (replies == reply.length) {endCallback();}
-            });
-        }
-    });
-}
--- users.js
+++ users.js
@@ -1,3 +1,17 @@
 // Module to manage users.
 // This primarily serves to keep track of who has which readings.
 var readings = require('./readings');
+
+// sends user names to userCallback, endCallback called at completion
+exports.listUsers = function(client, userCallback, endCallback) {
+    client.keys("user:*:name", function(err,reply){
+        var replies = 0;
+        for(var i=0; i < reply.length; i++) {
+            client.get(reply[i], function(err,name){
+                 userCallback(name);
+                replies++;
+                if (replies == reply.length) {endCallback();}
+            });
+        }
+    });
+}
