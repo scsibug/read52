5d93527690cb5c45506f989c0813d6ae0589de1f
Greg Heartsfield <scsibug@imap.cc>
2010-11-25 20:33:34 -0600

    Added tests for book lookup.

---
+++ tests/book_lookup-test.js
@@ -0,0 +1,27 @@
+var sys = require('sys'),
+    vows = require('vows'),
+    assert = require('assert');
+var book_lookup = require('../book_lookup');
+
+vows.describe('Amazon Book Lookup').addBatch({
+    'Lookup Book': {
+        topic: function () {
+            book_lookup.isbn_lookup("9780060733353",this.callback);
+        },
+        'has ASIN': function(err, book) {
+            assert.equal(book.ASIN, "0060733357");
+        },
+        'has Title': function(err, book) {
+            assert.equal(book.ItemAttributes.Title, "The Confusion (The Baroque Cycle, Vol. 2)");
+        },
+        'has ISBN': function(err, book) {
+            assert.equal(book.ItemAttributes.ISBN, "0060733357");
+        },
+        'has Author': function(err, book) {
+            assert.equal(book.ItemAttributes.Author, "Neal Stephenson");
+        },
+        'has NumberOfPages': function(err, book) {
+            assert.equal(book.ItemAttributes.NumberOfPages, "848");
+        }
+    }
+}).export(module);
