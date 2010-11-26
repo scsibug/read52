1a30f0cc7388716a8afd70a5812fc9fb5f073feb
Greg Heartsfield <scsibug@imap.cc>
2010-11-25 19:20:23 -0600

    Redis client created for use in tests.

--- tests/books-test.js
+++ tests/books-test.js
@@ -2,12 +2,25 @@ var vows = require('vows'),
     assert = require('assert');
 
 var books = require('../books');
+var redis = require("redis"),
+    client = redis.createClient();
+client.select(99,function() {}); // UNIT TEST database
 
 vows.describe('Books').addBatch({
-    'A book': {
+    'Create Book': {
+        topic: {title: "book title"},
+        'has title': function(book) {
+            assert.equal(book.title, "book title");
+        }
+    }
+}).addBatch({
+    'Retrieve Existing Book': {
         topic: {title: "book title"},
         'has title': function(book) {
             assert.equal(book.title, "book title");
         }
     }
 }).export(module);
+
+// Shutdown redis client
+client.quit();
