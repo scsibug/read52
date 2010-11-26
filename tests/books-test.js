a7faccd6e1903239f7a241ac43b046aba0953fca
Greg Heartsfield <scsibug@imap.cc>
2010-11-25 21:27:10 -0600

    Added tests for books module.

--- tests/books-test.js
+++ tests/books-test.js
@@ -1,26 +1,35 @@
-var vows = require('vows'),
+var sys = require('sys'),
+    vows = require('vows'),
     assert = require('assert');
-
 var books = require('../books');
 var redis = require("redis"),
     client = redis.createClient();
-client.select(99,function() {}); // UNIT TEST database
+
+client.select(99,function(err,result) {
+    // Clear out this test database
+    client.flushdb();
+});
 
 vows.describe('Books').addBatch({
     'Create Book': {
-        topic: {title: "book title"},
-        'has title': function(book) {
-            assert.equal(book.title, "book title");
-        }
-    }
-}).addBatch({
-    'Retrieve Existing Book': {
-        topic: {title: "book title"},
-        'has title': function(book) {
-            assert.equal(book.title, "book title");
-        }
+        topic: function() {
+            books.save_book(client,"9780060733353",this.callback);
+        },
+        'has ASIN': function(err,book) {
+            assert.equal(book.ASIN, "0060733357");
+        },
+        'has Title': function(err, book) {
+            assert.equal(book.ItemAttributes.Title, "The Confusion (The Baroque Cycle, Vol. 2)");
+        },
+        'has ISBN': function(err, book) {
+            assert.equal(book.ItemAttributes.ISBN, "0060733357");
+        },
+        'has Author': function(err, book) {
+            assert.equal(book.ItemAttributes.Author, "Neal Stephenson");
+        },
+        'has NumberOfPages': function(err, book) {
+            assert.equal(book.ItemAttributes.NumberOfPages, "848");
+        },
+        teardown: function() { client.quit()}
     }
 }).export(module);
-
-// Shutdown redis client
-client.quit();
