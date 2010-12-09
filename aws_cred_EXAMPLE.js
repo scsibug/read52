b32bf0d680df1131e0bd0b05b91aba93f2718679
Greg Heartsfield <scsibug@imap.cc>
2010-12-08 19:12:13 -0600

    Fixed aws_cred rename instruction.

--- aws_cred_EXAMPLE.js
+++ aws_cred_EXAMPLE.js
@@ -1,5 +1,5 @@
 // Amazon credentials should be specified here.
-// This file must then be renamed "apac_credentials.js"
+// This file must then be renamed "aws_cred.js"
 exports.awsId = '[AWS Account ID]';
 exports.awsSecret = '[AWS Secret]';
 exports.assocId = '[Tracking ID]';
