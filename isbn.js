f80d63042d1c9fb5092359669019c6b75d98af9a
Greg Heartsfield <scsibug@imap.cc>
2010-12-12 12:22:03 -0600

    Remove hyphens from ISBNs before converting.

--- isbn.js
+++ isbn.js
@@ -4,7 +4,7 @@ var _ = require("underscore");
 // Take ISBN-10 and ISBN-13 to well-formed (no dashes) ISBN-13 codes.
 // If the input cannot be reliably transformed, returns null.
 exports.to_isbn_13 = function (isbn) {
+    isbn = isbn.replace(/-/g,"");
     if (_.isNull(isbn) || _.isUndefined(isbn)) { return null; }
-    var parsed_isbn = isbnlib.ISBN.parse(isbn);
-    return (parsed_isbn ? parsed_isbn.codes.isbn13 : null);
+    return isbnlib.ISBN.asIsbn13(isbn,false)
 }
