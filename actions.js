315e74b513a84a438c0ae498396cf17d57ea3b78
Greg Heartsfield <scsibug@imap.cc>
2011-01-05 18:23:45 -0600

    Action log publishes links to users and readings.

--- actions.js
+++ actions.js
@@ -1,17 +1,29 @@
 var sys = require('sys');
 var _ = require('underscore');
 var users = require('./users');
+var readings = require('./readings');
 var rclient = require('./redisclient');
 
 var global_action_list = "actions";
 var list_max_size = 1000;
 var listener;
 
-exports.publish_action = function(user_id, action, callback) {
+exports.publish_action = function(user_id, action, object, callback) {
+    if (action === "read") {
+        publish_read_action(user_id,object,callback);
+    } else {
+        console.log("Unknown action:",action);
+        callback(null,null);
+    }
+};
+
+var publish_read_action = function(user_id, book, callback) {
     var client = rclient.getClient();
     users.get_by_id(user_id, function(err,user) {
         if(!_.isNull(user.name)) {
-            action = user.name + " " + action;
+            var user_link = "<a href='"+users.url_for_id(user_id)+"'>"+user.name+"</a>";
+            var read_link = "<a href='"+readings.url_for_id(user_id,book.id)+"'>"+book.title+"</a>";
+            action = user_link + " read " + read_link;
             if (!_.isUndefined(listener)) {
                 listener([action]);
             }
